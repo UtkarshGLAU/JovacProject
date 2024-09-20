32c53028f40264ca852348a1581c8bef67ad134c
UtkarshGLAU <122614349+UtkarshGLAU@users.noreply.github.com>
2024-09-20 19:22:58 +0530

    second commit

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
-console.log('Hugging Face API Key:', process.env.HUGGING_FACE_API_KEY);
+// console.log('Hugging Face API Key:', process.env.HUGGING_FACE_API_KEY);
 
 app.use(cors()); 
 app.use(bodyParser.json()); 
